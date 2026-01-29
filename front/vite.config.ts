f81b77b3e64663c75a0f78b7134b4c593a7bf079
jeffersonrodrigues343 <jeffersonk.alves@gmail.com>
2026-01-29 01:00:53 -0300

    config: ajusta scripts de deploy e corrige sintaxe no package.json

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CASSAB-solidario/', // Substitua pelo nome exato do seu repositório no GitHub
 })
