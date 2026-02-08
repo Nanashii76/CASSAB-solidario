75bde486175687b123b393de1381fd54614d2e32
nanashii76 <pauloamino76@gmail.com>
2026-02-07 22:11:53 -0300

    feat: Criando página de administração

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InvitePage from './pages/InvitePage';
+import AdminPage from './pages/AdminPage'; 
 
 function App() {
-    return <InvitePage />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<InvitePage />} />
+                <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
